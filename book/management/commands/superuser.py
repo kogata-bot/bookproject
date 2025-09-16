4d9029d130fd371a2a70c6d503e187462d75cb28
ogatakaito <ogata.kaito@power-next.co.jp>
2025-09-16 15:54:19 +0900

    change

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -1,4 +1,4 @@
-import os
+# import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="your_name").exists():
             User.objects.create_superuser(
-                username=os.environ.get("SUPERUSER_NAME"),
+                username='your_name',
                 email="",
-                password=os.environ.get("SUPERUSER_PASS"),
+                password='your_passwd',
             )
