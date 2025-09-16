d6ab90d1ce572073ec5f583bf35227d7fc909759
ogatakaito <ogata.kaito@power-next.co.jp>
2025-09-16 16:07:13 +0900

    refactor2

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tf#h5^n61t-k280y2+6&jh2(+mf*!h2j2!i+dpm9_zl(^$ge^0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
